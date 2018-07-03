955d6fc2b3050c92dc23b7af9f751a41136e66f5
bharatpatodi <bharatpatodi@gmail.com>
2018-07-03 18:49:14 +0530

    Added the newsAPI and started receiving the data with a simple dynamic display. Added a config.js to hide the api and included it in the gitignore.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,31 @@
-window.onload = init;
+const app = document.getElementById('root');
+const xhr = new XMLHttpRequest();
 
-function init() {
-  const fakeVar = 10;
-  alert("Hey");
-}
+// So, we just reference other js objects in the same folder like this.
+const myKey = config.MY_NEWSAPI_KEY;
+xhr.open('GET', 'https://newsapi.org/v2/top-headlines?country=us&apikey=' + myKey, true);
+xhr.onload = function() {
+    
+    let data = JSON.parse(xhr.response);
+    data.articles.forEach( function(newsItem) {
+
+        let card = document.createElement('div');
+        card.setAttribute('class', 'card'); 
+        app.appendChild(card);
+
+        let date = document.createElement('div');
+        date.setAttribute('class', 'date');
+        card.appendChild(date);
+
+        let title = document.createElement('div');
+        title.setAttribute('class', 'title');
+        card.appendChild(title);
+
+        // console.log( newsItem.title);
+        // card.innerHTML = newsItem.title;
+        date.textContent = newsItem.publishedAt;
+        title.textContent = newsItem.title;
+    });
+};
+xhr.send();
+
